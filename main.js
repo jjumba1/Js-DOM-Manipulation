8a22f037b496a346b016ddac6f91cb54f9eb7f8d
Jjumba Stephen <jjumba1@gmail.com>
2021-05-11 17:35:46 +0300

    Don Manipulation

---
+++ main.js
@@ -0,0 +1,56 @@
+
+function myFunctionPot() {
+
+    document.getElementById('pot').style.display='block';
+    document.getElementById('dog').style.display='none';
+    document.getElementById('david').style.display='none';
+    document.getElementById('cat').style.display='none';
+    document.getElementById('display').style.display='none';
+    
+}
+function myFunctionCat() {
+
+    document.getElementById('pot').style.display='none';
+    document.getElementById('dog').style.display='none';
+    document.getElementById('david').style.display='none';
+    document.getElementById('cat').style.display='block';
+    document.getElementById('display').style.display='none';
+    
+}
+function myFunctionDavid() {
+
+    document.getElementById('pot').style.display='none';
+    document.getElementById('dog').style.display='none';
+    document.getElementById('david').style.display='block';
+    document.getElementById('cat').style.display='none';
+    document.getElementById('display').style.display='none';
+    
+}
+function myFunctionDog() {
+
+    document.getElementById('pot').style.display='none';
+    document.getElementById('dog').style.display='block';
+    document.getElementById('david').style.display='none';
+    document.getElementById('cat').style.display='none';
+    document.getElementById('display').style.display='none';
+    
+}
+
+function myFunctionAppear() {
+
+    document.getElementById('pot').style.display='none';
+    document.getElementById('dog').style.display='none';
+    document.getElementById('david').style.display='none';
+    document.getElementById('cat').style.display='none';
+    document.getElementById('display').style.display='block';
+    
+}
+
+function myFunctionTry() {
+
+    document.getElementById('try').style.display='block';
+    
+}
+
+
+
